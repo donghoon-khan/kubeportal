7d641fb1f73726f898a2775fdf9f6ce5bebcaba1
donghoon-khan <mustwin@kakao.com>
2021-01-13 17:51:55 +0900

    feature: google oauth2 기반 인증 추가

--- go.mod
+++ go.mod
@@ -1,9 +1,5 @@
 module kubernetes-portal
 
-go 1.14
+go 1.15
 
-require (
-	github.com/gin-gonic/gin v1.6.3
-	github.com/gorilla/sessions v1.2.1 // indirect
-	golang.org/x/oauth2 v0.0.0-20210112200429-01de73cf58bd // indirect
-)
+require golang.org/x/oauth2 v0.0.0-20210112200429-01de73cf58bd
--- src/app/backend/portal.go
+++ src/app/backend/portal.go
@@ -1,8 +1,71 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var (
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:3000/auth/google/callback",
+		ClientID:     "486448652557-vpb1117efkn2ca2fan2ud20r3lgp5mto.apps.googleusercontent.com",
+		ClientSecret: "I20bP76xC0HSq7TCcK03pVik",
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+	// TODO: randomize it
+	randomState = "random"
+)
 
 func main() {
-	router := gin.Default()
-	router.Run()
+	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/login", handleLogin)
+	http.HandleFunc("/callback", handleCallback)
+
+	http.ListenAndServe(":3000", nil)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	var html = `<html><body><a href="/login">Google Login</a></body></html>`
+	fmt.Fprint(w, html)
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConfig.AuthCodeURL(randomState)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func handleCallback(w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("state") != randomState {
+		fmt.Println("state is not valid")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	token, err := googleOauthConfig.Exchange(oauth2.NoContext, r.FormValue("code"))
+	if err != nil {
+		fmt.Println("could not get token: %s", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?accesstoken=" + token.AccessToken)
+	if err != nil {
+		fmt.Println("could not get request: %s", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("could not parse response: %s", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Fprintf(w, "Response: %s", content)
 }
