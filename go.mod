c1d85f2176501ba3d923c1e4a58b39484b9e94fb
donghoon-khan <mustwin@kakao.com>
2021-01-13 22:19:44 +0900

    fix: proj 정리

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module kubernetes-portal
+module github.com/donghoon-khan/kubernetes-portal
 
 go 1.15
 
-require golang.org/x/oauth2 v0.0.0-20210112200429-01de73cf58bd
+require (
+	github.com/gorilla/mux v1.8.0
+	golang.org/x/oauth2 v0.0.0-20210112200429-01de73cf58bd
+)
--- src/app/backend/portal.go
+++ src/app/backend/portal.go
@@ -1,71 +1,88 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/gorilla/mux"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
-var (
-	googleOauthConfig = &oauth2.Config{
-		RedirectURL:  "http://localhost:3000/auth/google/callback",
-		ClientID:     "486448652557-vpb1117efkn2ca2fan2ud20r3lgp5mto.apps.googleusercontent.com",
-		ClientSecret: "I20bP76xC0HSq7TCcK03pVik",
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-		Endpoint:     google.Endpoint,
-	}
-	// TODO: randomize it
-	randomState = "random"
-)
-
-func main() {
-	http.HandleFunc("/", handleHome)
-	http.HandleFunc("/login", handleLogin)
-	http.HandleFunc("/callback", handleCallback)
-
-	http.ListenAndServe(":3000", nil)
+var googleOauthConfig = oauth2.Config{
+	RedirectURL:  "http://localhost:3000/auth/google/callback",
+	ClientID:     "486448652557-vpb1117efkn2ca2fan2ud20r3lgp5mto.apps.googleusercontent.com",
+	ClientSecret: "I20bP76xC0HSq7TCcK03pVik",
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+	Endpoint:     google.Endpoint,
 }
 
-func handleHome(w http.ResponseWriter, r *http.Request) {
-	var html = `<html><body><a href="/login">Google Login</a></body></html>`
-	fmt.Fprint(w, html)
-}
-
-func handleLogin(w http.ResponseWriter, r *http.Request) {
-	url := googleOauthConfig.AuthCodeURL(randomState)
+func googleLoginHandler(w http.ResponseWriter, r *http.Request) {
+	state := generateStateOauthCookie(w)
+	url := googleOauthConfig.AuthCodeURL(state)
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
-func handleCallback(w http.ResponseWriter, r *http.Request) {
-	if r.FormValue("state") != randomState {
-		fmt.Println("state is not valid")
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-	token, err := googleOauthConfig.Exchange(oauth2.NoContext, r.FormValue("code"))
-	if err != nil {
-		fmt.Println("could not get token: %s", err.Error())
+func generateStateOauthCookie(w http.ResponseWriter) string {
+	expiration := time.Now().Add(1 * 1 * time.Hour)
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := &http.Cookie{Name: "oauthstate", Value: state, Expires: expiration}
+	http.SetCookie(w, cookie)
+	return state
+}
+
+func googleAuthCallback(w http.ResponseWriter, r *http.Request) {
+	oauthstate, _ := r.Cookie("oauthstate")
+
+	if r.FormValue("state") != oauthstate.Value {
+		log.Printf("invalid google oauth state cookie:%s state:%s\n", oauthstate.Value, r.FormValue("state"))
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
 
-	resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?accesstoken=" + token.AccessToken)
+	data, err := getGoogleUserInfo(r.FormValue("code"))
 	if err != nil {
-		fmt.Println("could not get request: %s", err.Error())
+		log.Println(err.Error())
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
 
-	defer resp.Body.Close()
-	content, err := ioutil.ReadAll(resp.Body)
+	fmt.Fprint(w, string(data))
+}
+
+func getGoogleUserInfo(code string) ([]byte, error) {
+	token, err := googleOauthConfig.Exchange(context.Background(), code)
 	if err != nil {
-		fmt.Println("could not parse response: %s", err.Error())
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
+		return nil, fmt.Errorf("Failed to Exchange %s\n", err.Error())
 	}
 
-	fmt.Fprintf(w, "Response: %s", content)
+	resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to Get UserInfo %s\n", err.Error())
+	}
+
+	return ioutil.ReadAll(resp.Body)
+}
+
+func main() {
+
+	log.SetOutput(os.Stdout)
+
+	router := mux.NewRouter()
+
+	router.Handle("/", http.FileServer(http.Dir("../frontend/")))
+	router.HandleFunc("/auth/google/login", googleLoginHandler)
+	router.HandleFunc("/auth/google/callback", googleAuthCallback)
+
+	http.ListenAndServe(":3000", router)
 }
